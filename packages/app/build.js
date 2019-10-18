36c92e81fcf41df5486fccf189680a70f4afc48b
Ricardo Canelas <ricardocanelas@gmail.com>
2019-10-18 20:47:17 -0300

    feat: remove css-files

--- packages/app/build.js
+++ packages/app/build.js
@@ -6,9 +6,10 @@ const jsFiles = glob.sync('build/static/?(js)/*.?(js)').map(filepath => {
   return path.resolve(__dirname, filepath)
 })
 
-const cssFiles = glob.sync('build/static/?(css)/*.?(css)').map(filepath => {
-  return path.resolve(__dirname, filepath)
-})
+// [deprecated]
+// const cssFiles = glob.sync('build/static/?(css)/*.?(css)').map(filepath => {
+//   return path.resolve(__dirname, filepath)
+// })
+// // mergeFiles(cssFiles, __dirname + '/../simplest-cms/bundle.css').then(status => {})
 
 mergeFiles(jsFiles, __dirname + '/../simplest-cms/bundle.js').then(status => {})
-mergeFiles(cssFiles, __dirname + '/../simplest-cms/bundle.css').then(status => { })
