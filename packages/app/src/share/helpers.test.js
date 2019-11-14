012df64ef3e8d4d22502be84b9f3328f5933ef4f
Ricardo Canelas <ricardocanelas@gmail.com>
2019-11-14 13:15:00 -0300

    test: add test for stringToBoolean and removeQuote

--- packages/app/src/share/helpers.test.js
+++ packages/app/src/share/helpers.test.js
@@ -1,4 +1,4 @@
-import { extractArguments } from './helpers'
+import { extractArguments, stringToBoolean, removeQuote } from './helpers'
 
 describe('extractArguments', () => {
   it('checks the args', () => {
@@ -13,5 +13,34 @@ describe('extractArguments', () => {
     expect(args.length).toBe(2)
     expect(args[0]).toBe(`'y'`)
     expect(args[1]).toBe(`"z"`)
+  })
+})
+
+describe('stringToBoolean', () => {
+  it('checks conditions', () => {
+    expect(stringToBoolean('"any"')).toBe(true)
+    expect(stringToBoolean('any')).toBe(true)
+
+    expect(stringToBoolean('yes')).toBe(true)
+    expect(stringToBoolean('1')).toBe(true)
+    expect(stringToBoolean(1)).toBe(true)
+    expect(stringToBoolean('true')).toBe(true)
+    expect(stringToBoolean('TRue')).toBe(true)
+    expect(stringToBoolean(true)).toBe(true)
+
+    expect(stringToBoolean('no')).toBe(false)
+    expect(stringToBoolean('0')).toBe(false)
+    expect(stringToBoolean(0)).toBe(false)
+    expect(stringToBoolean('false')).toBe(false)
+    expect(stringToBoolean('faLSE')).toBe(false)
+    expect(stringToBoolean(false)).toBe(false)
+  })
+})
+
+describe('removeQuote', () => {
+  it('checks conditions', () => {
+    expect(removeQuote('"any"')).toBe('any')
+    expect(removeQuote("'any'")).toBe('any')
+    expect(removeQuote('any')).toBe('any')
   })
 })
