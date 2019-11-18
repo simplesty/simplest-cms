81c8ee8b0e5c61ee86b2e031c508875547cab3af
Ricardo Canelas <ricardocanelas@gmail.com>
2019-11-18 10:03:59 -0300

    feat: display boolean value

--- packages/app/src/components/collectionIndex.js
+++ packages/app/src/components/collectionIndex.js
@@ -9,6 +9,7 @@ import Paper from '@material-ui/core/Paper'
 import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import isBoolean from 'lodash/isBoolean'
 import actions from '../redux/actions'
 
 const useStyles = makeStyles({
@@ -20,7 +21,22 @@ const useStyles = makeStyles({
     marginBottom: 16,
   },
   table: {
-    minWidth: 650,
+    minWidth: 450,
+  },
+  buttons: {
+    padding: '3px 6px',
+    minWidth: 'auto',
+    fontSize: '0.6rem',
+  },
+  cell: {
+    actions: {
+      width: 150,
+      minWidth: 145,
+      root: {
+        width: 150,
+        minWidth: 145,
+      },
+    },
   },
 })
 
@@ -47,7 +63,7 @@ const CollectionIndex = props => {
               {props.collection.fields.map(field => {
                 return <TableCell key={field.name}>{field.name}</TableCell>
               })}
-              <TableCell align="right"></TableCell>
+              <TableCell align="right" style={{ minWidth: 130 }}></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -60,24 +76,35 @@ const CollectionIndex = props => {
                   </TableCell>
 
                   {props.collection.fields.map(field => {
+                    const value = isBoolean(item[field.name])
+                      ? item[field.name]
+                        ? 'true'
+                        : 'false'
+                      : item[field.name]
                     return (
                       <TableCell key={field.name} component="th" scope="row">
-                        {item[field.name]}
+                        {value}
                       </TableCell>
                     )
                   })}
 
-                  <TableCell align="right">
+                  <TableCell align="right" className={classes.cell.actions}>
                     <Button
                       size="small"
                       variant="outlined"
                       to={`/collections/${props.match.params.collection}/${item._uid}`}
                       component={Link}
                       style={{ marginRight: 6 }}
+                      className={classes.buttons}
                     >
                       Edit
                     </Button>
-                    <Button size="small" variant="outlined" onClick={handleDelete(item)}>
+                    <Button
+                      size="small"
+                      variant="outlined"
+                      className={classes.buttons}
+                      onClick={handleDelete(item)}
+                    >
                       Delete
                     </Button>
                   </TableCell>
