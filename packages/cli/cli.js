45a643b14349c57abe8574b5303896b539ba6405
Ricardo Canelas <ricardocanelas@gmail.com>
2019-11-21 17:18:22 -0300

    feat: update cli 0.1.3

--- packages/cli/cli.js
+++ packages/cli/cli.js
@@ -20,19 +20,20 @@ program.parse(process.argv)
 program.on('--help', function() {
   console.log('')
   console.log('Examples:')
-  console.log('  npx github:ricardocanelas/simplest-cms myadmin')
+  console.log('  npx github:simplest-cms/simplest-cms myadmin')
   console.log('')
 })
 
 if (typeof projectName === 'undefined') {
-  console.error('Please specify the project directory:')
-  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`)
-  console.log()
-  console.log('For example:')
-  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('cms')}`)
-  console.log()
-  console.log(`Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`)
-  process.exit(1)
+  projectName = '.' // current directory
+  // console.error('Please specify the project directory:')
+  // console.log(`  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`)
+  // console.log()
+  // console.log('For example:')
+  // console.log(`  ${chalk.cyan(program.name())} ${chalk.green('cms')}`)
+  // console.log()
+  // console.log(`Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`)
+  // process.exit(1)
 }
 
 const dir = process.cwd() + '/' + projectName
@@ -55,6 +56,6 @@ files.forEach(file => {
 })
 
 log(chalk.black('Installed with success in ./' + projectName + ' directory'))
-log(chalk.white.bgBlack(' Please, now you need to edit the file: '))
-log(chalk.black('- config.php'))
+log(chalk.white.bgBlack(' Now, you need to edit the file: "config.php" '))
+// log(chalk.black('- config.php'))
 log('')
